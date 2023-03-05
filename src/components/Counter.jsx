6e6f8d6887fa0fc97ae0863ed8ef8b479b273c43
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-03-04 22:23:52 -0600

    Fragment

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,12 @@ const Counter = () => {
 
     const [counter, setCounter] = useState(5); // [estado, cambioEstado() ]
 
-    return <h1>Counter: { counter }</h1>
+    return (
+        <>
+            <h1>Counter: { counter }</h1>
+            <button>Add</button>
+        </>
+    )
 }
 
 
