507853bbd379884c7ac13140ff9867b5b99a9174
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-03-09 22:00:02 -0600

    Pintando Información del API REST

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 
+import './Users.css';
+
 export const Users = () => {
 	const [users, setUsers] = useState([]);
 
 	const getUsuarios = async () => {
 		const response = await fetch("https://jsonplaceholder.typicode.com/users");
 		const data = await response.json();
-		
+
 		console.log(data);
 
 		setUsers(data);
@@ -16,9 +18,38 @@ export const Users = () => {
 		getUsuarios();
 	}, []);
 
-	return (
-		<div>Users</div>
-	)
+	return <>
+		<h3>Users</h3>
+
+		{/* <ul>
+			{users.map(user => <li key={user.id}>{user.name} - {user.email} </li>)}
+		</ul> */}
+
+		<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Username</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Website</th>
+				</tr>
+			</thead>
+			<tbody>
+				{users.map(user =>(
+					<tr key={user.id}>
+						<td>{user.id}</td>
+						<td>{user.name}</td>
+						<td>{user.username}</td>
+						<td>{user.phone}</td>
+						<td>{user.email}</td>
+						<td><a href={user.website} target="_blank">{user.website}</a></td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	</>
 }
 
 export default Users;
